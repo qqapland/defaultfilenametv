8accdf101ed924386d8bbff879de84fe7b57de65
qqap <lavahumour@gmail.com>
2024-08-09 12:52:54 +0530

    patch

--- main.go
+++ main.go
@@ -1,22 +1,81 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	firstPart := []string{"DSC", "MOV", "IMG", "100", "MVI"}
+	separator := []string{" ", "_", ""}
+
+	randomFirstPart := firstPart[rand.Intn(len(firstPart))]
+	randomSeparator := separator[rand.Intn(len(separator))]
+	numberBase := rand.Intn(9999)
+
+	padToFour := func(number int) string {
+		return fmt.Sprintf("%04d", number)
+	}
+
+	numbers := padToFour(numberBase)
+
+	randomQuery := randomFirstPart + randomSeparator + numbers
+
+	videoID, err := searchRandomVideo(randomQuery)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"videoId": videoID})
+}
+
+func searchRandomVideo(query string) (string, error) {
+	apiKey := os.Getenv("YT_API_KEY")
+	url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=%s&key=%s", url.QueryEscape(query), apiKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var data struct {
+		Items []struct {
+			ID struct {
+				VideoID string `json:"videoId"`
+			} `json:"id"`
+		} `json:"items"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return "", fmt.Errorf("error decoding response: %v", err)
+	}
+
+	if len(data.Items) == 0 {
+		return "", fmt.Errorf("no videos found")
+	}
+
+	randomIndex := rand.Intn(len(data.Items))
+	return data.Items[randomIndex].ID.VideoID, nil
+}
+
 func main() {
-	fmt.Println("App Starting...")
+	fmt.Println("Hello go...")
 
 	fmt.Println("YT_API_KEY:", os.Getenv("YT_API_KEY"))
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":3000", nil)) // 3000 here
-}
-func handler(w http.ResponseWriter, r *http.Request) {
-	simpleOutput := fmt.Sprintf("Got hit from: %s", r.URL.Path[1:])
-	fmt.Println(simpleOutput)
-	fmt.Fprintf(w, simpleOutput)
+	http.HandleFunc("/random", randomHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
