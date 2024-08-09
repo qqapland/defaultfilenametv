fad6ec0399feff3af3337d785c6d91c337bc8873
qqap <lavahumour@gmail.com>
2024-08-09 10:55:46 +0530

    patch

--- deploydefaulttv.sh
+++ deploydefaulttv.sh
@@ -1,3 +1,5 @@
-docker build -t defaultfilenametv -f Dockerfile .    
-
+dsi() { docker stop $(docker ps -a | awk -v i="^$1.*" '{if($2~i){print$1}}'); }
+dsi defaultfilenametv
+docker rmi -f $(docker images -q defaultfilenametv) # remove the old image
+docker load -i /home/a/defaultfilenametv.tar # this causes rename
 docker run -it --rm -p 3004:3000 defaultfilenametv
